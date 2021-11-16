cc1fe11666481abf6e0480f1e2b38fbd64244d0f
masai <mahapamasai@gmail.com>
2021-11-16 16:10:12 +0200

    added files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField:""
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then( response => response.json())
+    .then(users => this.setState({"monsters": users}))
+  }
+
+  //arrow functions have lexical scope. context. when constructor runs, it binds this function
+  handleChange = (e) =>{
+      //set state is async. so in order to wait for it before we print out
+      this.setState({searchField: e.target.value}, () =>
+      console.log(this.state)
+      )
+  }
+
+  render(){
+    //destructuring
+    const {monsters, searchField} = this.state;
+    const fiteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    
+   return <div className="App">
+   <h1>Monster Rolodex</h1>
+    <SearchBox 
+      placeholder="find monster"
+      handleChange={this.handleChange}
+    />
+      {<CardList monsters={fiteredMonsters}/>}
     </div>
-  );
+  }
 }
-
 export default App;
